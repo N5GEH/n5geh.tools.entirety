24cb29812284a09426dfea079b72b86cfdd1c4f7
dnikolay-ebc <daniel.nikolay@rwth-aachen.de>
2022-06-28 13:33:02 +0200

    fix: show sidebar tooltips only on small screen

---
+++ app/Entirety/static/js/main.js
@@ -0,0 +1,4 @@
+;(function () {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+})()
