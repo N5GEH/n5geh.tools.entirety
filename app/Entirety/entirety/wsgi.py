5499f10625cc02d77c36de2126b72df09ad78997
dnikolay-ebc <daniel.nikolay@rwth-aachen.de>
2022-06-30 00:36:10 +0200

    feat: use pydantic settings

--- app/Entirety/entirety/wsgi.py
+++ app/Entirety/entirety/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
 from django.core.wsgi import get_wsgi_application
+from pydantic_settings import SetUp
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "entirety.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "entirety.settings")
 
+
+SetUp().configure()
 application = get_wsgi_application()
