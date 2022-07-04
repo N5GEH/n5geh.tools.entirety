df4fcbb9cfeeb53854cd7b565c5afc3442219dd2
dnikolay-ebc <daniel.nikolay@rwth-aachen.de>
2022-07-04 22:23:58 +0200

    chore: add user model meta info to database

--- app/Entirety/users/models.py
+++ app/Entirety/users/models.py
@@ -3,5 +3,13 @@ from django.db import models
 
 
 class User(AbstractUser):
-    is_server_admin = models.BooleanField(default=False)
-    is_project_admin = models.BooleanField(default=False)
+    is_server_admin = models.BooleanField(
+        default=False,
+        help_text="User can create and update projects for any project admin.",
+        verbose_name="server admin status",
+    )
+    is_project_admin = models.BooleanField(
+        default=False,
+        help_text="User can create projects and update self created.",
+        verbose_name="project admin status",
+    )
