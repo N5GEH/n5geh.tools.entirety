aa402f92fd85a57b8b6d03981c9d1a2ba81be4b7
Saira Bano <saira.bano@eonerc.rwth-aachen.de>
2022-07-14 09:55:11 +0200

    fix: corrected braces

--- app/Entirety/projects/forms.py
+++ app/Entirety/projects/forms.py
@@ -14,6 +14,6 @@ class ProjectForm(forms.ModelForm):
 
     class Meta:
         model = Project
-        fields = {'name', 'description', 'fiware_service', 'webpage_url','logo'}
+        fields = ['name', 'description', 'fiware_service', 'webpage_url','logo']
 
 
