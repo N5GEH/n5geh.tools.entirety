c305e10c658696ec0f6ad7fb8ea39c35c0fdc7f0
Saira Bano <saira.bano@eonerc.rwth-aachen.de>
2022-07-19 11:25:34 +0200

    chore: project mixins

--- app/Entirety/projects/mixins.py
+++ app/Entirety/projects/mixins.py
@@ -1,25 +1,14 @@
-from django.core.exceptions import PermissionDenied
-from projects.models import Project
-from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 
-class ProjectContextMixin:
-    project = None
+class ProjectSelfMixin(UserPassesTestMixin):
+    def test_func(self):
+        obj = self.get_object()
+        return (
+            self.request.user.is_project_admin and obj.owner == self.request.user
+        ) or self.request.user.is_server_admin
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
 
-        context["project"] = self.project
-
-        return context
-
-    def dispatch(self, request, *args, **kwargs):
-        self.project = get_object_or_404(Project, pk=kwargs.get("project_id", None))
-
-        if not (
-            self.project.is_owner(user=request.user)
-            or self.project.is_user(user=request.user)
-        ):
-            raise PermissionDenied()
-
-        return super().dispatch(request, *args, **kwargs)
+class ProjectCreateMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_project_admin or self.request.user.is_server_admin
--- app/Entirety/projects/views.py
+++ app/Entirety/projects/views.py
@@ -1,42 +1,53 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic import View
-from django.views.generic.edit import UpdateView, CreateView, \
-    DeleteView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
 
 from .forms import ProjectForm
+from .mixins import ProjectCreateMixin, ProjectSelfMixin
 from .models import Project
 
 
-class Index(View):
-    def get(self, request):
+class Index(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        user = request.user
         projects = Project.objects.filter(
-            name__icontains=request.GET.get('search', default=""))
-        context = {'project_list': projects}
-        return render(request, 'projects/index.html', context)
+            name__icontains=request.GET.get("search", default="")
+        )
+        context = {
+            "project_list": projects,
+            "add_project": (user.is_project_admin or user.is_server_admin),
+        }
+        return render(request, "projects/index.html", context)
 
 
-class Update(UpdateView):
+class Update(LoginRequiredMixin, ProjectSelfMixin, UpdateView):
     model = Project
-    template_name = 'projects/detail.html'
+    template_name = "projects/detail.html"
     form_class = ProjectForm
 
     def get_success_url(self):
         return reverse("projects:index")
 
 
-class Create(CreateView):
+class Create(LoginRequiredMixin, ProjectCreateMixin, CreateView):
     model = Project
-    template_name = 'projects/detail.html'
+    template_name = "projects/detail.html"
     form_class = ProjectForm
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.owner = self.request.user
+        return super(Create, self).form_valid(form)
 
     def get_success_url(self):
         return reverse("projects:index")
 
 
-class Delete(DeleteView):
+class Delete(LoginRequiredMixin, ProjectSelfMixin, DeleteView):
     model = Project
-    template_name = 'projects/index.html'
+    template_name = "projects/index.html"
 
     def get_success_url(self):
         return reverse("projects:index")
