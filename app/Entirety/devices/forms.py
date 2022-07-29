08fc47233c8e970362bd9eec21e24f0a46922d78
JunsongDu <junsong.du@eonerc.rwth-aachen.de>
2022-07-29 10:42:27 +0200

    feat: implement device creation

--- app/Entirety/devices/forms.py
+++ app/Entirety/devices/forms.py
@@ -1,88 +1,40 @@
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, Fieldset, Row, Column
 from django import forms
+from django.forms import formset_factory
 
 
-class DeviceDetailForm(forms.Form):
-    # Basis Info
-    device_id = forms.CharField(label="Device ID", max_length=100)
-    device_id.group = "basis"
-    entity_name = forms.CharField(label="Entity Name", max_length=100)
-    entity_name.group = "basis"
-    entity_type = forms.CharField(label="Entity Type", max_length=100)
-    entity_type.group = "basis"
-
-    basis_fields = [device_id, entity_name, entity_type]
+class DeviceBasic(forms.Form):
+    device_id = forms.CharField(label="Device ID", max_length=100, required=True)
+    entity_name = forms.CharField(label="Entity Name", max_length=100, required=True)
+    entity_type = forms.CharField(label="Entity Type", max_length=100, required=True)
 
     def __init__(self, *args, **kwargs):
-        super(DeviceDetailForm, self).__init__(*args, **kwargs)
-
-        # parse the commands/attributes dict from initial
-        attributes = self.initial.get("attributes")
-        commands = self.initial.get("commands")
-
-        # initialize the attributes fields
-        if attributes:
-            self._init_attributes_group(attributes=attributes)
-
-        # TODO implement commands initialization
-
+        super().__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         self.helper.form_tag = False
-        self.helper.layout.append(Submit(name="save", value="Save"))
 
-    def _init_attributes_group(self, attributes):
-        for i, attribute in enumerate(attributes):
-            field_prefix = f"attribute_{i + 1}"
 
-            # create fields for an attribute
-            field_name_attribute_name = f"{field_prefix}_name"
-            self.fields[field_name_attribute_name] = forms.CharField(
-                required=False, label="Name"
-            )
-            self.fields[field_name_attribute_name].group = field_prefix
-            self.initial[field_name_attribute_name] = attribute["name"]
+class DeviceAttributes(forms.Form):
+    name = forms.CharField(label="Name", required=True)
+    type = forms.CharField(label="Type", required=True)
+    object_id = forms.CharField(label="Object ID", required=True)
 
-            field_name_attribute_type = f"{field_prefix}_type"
-            self.fields[field_name_attribute_type] = forms.CharField(
-                required=False, label="Type"
-            )
-            self.fields[field_name_attribute_type].group = field_prefix
-            self.initial[field_name_attribute_type] = attribute["type"]
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_tag = False
 
-            field_name_attribute_object_id = f"{field_prefix}_object_id"
-            self.fields[field_name_attribute_object_id] = forms.CharField(
-                required=False, label="Object_id"
-            )
-            self.fields[field_name_attribute_object_id].group = field_prefix
-            self.initial[field_name_attribute_object_id] = attribute["object_id"]
 
-    def get_attribute_fields(self):
-        return [field for field in self if self.fields[field.name].group == "attribute"]
+class DeviceCommands(forms.Form):
+    name = forms.CharField(label="Name", required=True)
+    type = forms.CharField(label="Type", required=True)
 
-    def get_basis_fields(self):
-        return [field for field in self if self.fields[field.name].group == "basis"]
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_tag = False
 
-    def get_command_fields(self):
-        return [field for field in self if self.fields[field.name].group == "command"]
 
-    def get_attr_fieldsets(self):
-        fieldsets = []  # list of all attribute fieldset
-
-        for field_name in self.fields:
-            if field_name.startswith("attribute") and field_name.endswith("name"):
-                field_prefix = field_name.split("_name")[0]
-                # field set holds the fields of an attribute
-                fieldsets.append(
-                    {
-                        "alias": field_prefix,
-                        "fields": [
-                            self[
-                                f"{field_prefix}_name"
-                            ],  # creat bound field for template
-                            self[f"{field_prefix}_type"],
-                            self[f"{field_prefix}_object_id"],
-                        ],
-                    }
-                )
-        return fieldsets
+Attributes = formset_factory(DeviceAttributes, extra=1)
+Commands = formset_factory(DeviceCommands, extra=1)
