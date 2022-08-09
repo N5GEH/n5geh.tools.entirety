b8045868fc56170d4ff14e5b332eca6c97816119
Saira Bano <saira.bano@eonerc.rwth-aachen.de>
2022-08-09 10:48:48 +0200

    chore: project image clickable for detail view

--- app/Entirety/projects/urls.py
+++ app/Entirety/projects/urls.py
@@ -6,6 +6,7 @@ app_name = "projects"
 urlpatterns = [
     path("", views.Index.as_view(), name="index"),
     path("create", views.Create.as_view(), name="create"),
+    path("<str:pk>/detail", views.Detail.as_view(), name="detail"),
     path("<str:pk>/update", views.Update.as_view(), name="update"),
     path("<str:pk>/delete", views.Delete.as_view(), name="delete"),
     path("<str:project_id>/alarming/", include("alarming.urls")),
