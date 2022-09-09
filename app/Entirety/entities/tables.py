657da3dfb7ca614523481c81d8bc7d2906aec187
Saira Bano <saira.bano@eonerc.rwth-aachen.de>
2022-09-09 16:18:26 +0200

    fix: table to include both id and type

--- app/Entirety/entities/tables.py
+++ app/Entirety/entities/tables.py
@@ -4,8 +4,15 @@ from entities.requests import get_entities_list
 
 
 class EntityTable(tables.Table):
+    # using & character as a splitter between id and type because this character is not allowed in NGSIv2 entity id
+    # and entity type
     selection = tables.columns.CheckBoxColumn(
-        accessor="id", attrs={"th__input": {"onclick": "toggle(this)"}}, orderable=False
+        accessor="id",
+        attrs={
+            "th__input": {"onclick": "toggle(this)"},
+            "td__input": {"value": lambda record: record.id + "&" + record.type},
+        },
+        orderable=False,
     )
     id = tables.columns.Column()
     type = tables.columns.Column()
