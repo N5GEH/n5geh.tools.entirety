e194cf733a7cbd125c38f3884834c18e2b6dbc7a
Saira Bano <saira.bano@eonerc.rwth-aachen.de>
2022-09-09 09:04:03 +0200

    chore: delete entity url inclusion

--- app/Entirety/entities/urls.py
+++ app/Entirety/entities/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from entities.views import EntityList, Update, Create
+from entities.views import EntityList, Update, Create, Delete
 
 app_name = "entities"
 urlpatterns = [
     path("", EntityList.as_view(), name="list"),
     path("<str:entity_id>/<str:entity_type>/update/", Update.as_view(), name="update"),
     path("create/", Create.as_view(), name="create"),
+    path("<str:entity_id>/delete/", Delete.as_view(), name="delete"),
 ]
