cc24dfe7ea271576292b70236d3da5895635fbe4
JunsongDu <junsong.du@eonerc.rwth-aachen.de>
2022-09-22 16:45:46 +0200

    chore: unify the list view

--- app/Entirety/devices/tables.py
+++ app/Entirety/devices/tables.py
@@ -3,7 +3,15 @@ from django_tables2 import tables
 
 class DevicesTable(tables.Table):
     selection = tables.columns.CheckBoxColumn(
-        accessor="device_id", attrs={"th__input": {"onclick": "toggle(this)"}}, orderable=False
+        verbose_name="Select",
+        accessor="device_id",
+        attrs={
+            "td__input": {
+                "value": lambda record: record.device_id,
+                "type": "radio",
+            },
+        },
+        orderable=False,
     )
     device_id = tables.columns.Column()
     entity_name = tables.columns.Column()
