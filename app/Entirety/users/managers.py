3989054febe61ca78b6c68443d1004412098a8d9
dnikolay-ebc <daniel.nikolay@rwth-aachen.de>
2022-10-10 19:05:29 +0200

    fix: use admin for user creation

---
+++ app/Entirety/users/managers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, username, email, password=None):
+        if email is None:
+            raise TypeError("Users must have an email address.")
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, email, password):
+        user = self.create_user(
+            email=email,
+            username=username,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_server_admin = True
+        user.is_project_admin = True
+        user.save(using=self._db)
+
+        return user
