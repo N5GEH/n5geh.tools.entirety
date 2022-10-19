31b67d5a4978372f5999c4946f46c95e39f44768
Saira Bano <saira.bano@eonerc.rwth-aachen.de>
2022-10-19 16:12:46 +0200

    chore: added users to project option

--- app/Entirety/projects/forms.py
+++ app/Entirety/projects/forms.py
@@ -1,6 +1,7 @@
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django import forms
+from users.models import User
 from .models import Project
 
 
@@ -10,44 +11,70 @@ class ProjectForm(forms.ModelForm):
 
         self.helper = FormHelper(self)
 
+        self.fields["owner"].widget.attrs = {
+            "data-bs-toggle": "tooltip",
+            "data-bs-placement": "left",
+            "title": "Owner can not be edited. It is assigned automatically on project creation.",
+            "disabled": True,
+        }
+
+        self.fields["users"].widget = forms.CheckboxSelectMultiple(
+            attrs={
+                "data-bs-toggle": "tooltip",
+                "data-bs-placement": "left",
+                "title": "Include or exclude users into project",
+            }
+        )
+        self.fields["users"].queryset = User.objects.exclude(
+            id=self.instance.owner_id
+        ).filter(is_server_admin=False)
+
         self.helper.layout.append(Submit(name="save", value="Save"))
 
     class Meta:
         model = Project
-        fields = ["name", "description", "fiware_service", "webpage_url", "logo"]
+        fields = [
+            "name",
+            "description",
+            "fiware_service",
+            "webpage_url",
+            "logo",
+            "owner",
+            "users",
+        ]
         widgets = {
             "name": forms.TextInput(
                 attrs={
                     "data-bs-toggle": "tooltip",
-                    "data-bs-placement": "top",
+                    "data-bs-placement": "left",
                     "title": "Project name",
                 }
             ),
             "description": forms.TextInput(
                 attrs={
                     "data-bs-toggle": "tooltip",
-                    "data-bs-placement": "top",
+                    "data-bs-placement": "left",
                     "title": "Project description",
                 }
             ),
             "fiware_service": forms.TextInput(
                 attrs={
                     "data-bs-toggle": "tooltip",
-                    "data-bs-placement": "top",
+                    "data-bs-placement": "left",
                     "title": "Fiware service",
                 }
             ),
             "webpage_url": forms.URLInput(
                 attrs={
                     "data-bs-toggle": "tooltip",
-                    "data-bs-placement": "top",
+                    "data-bs-placement": "left",
                     "title": "Project webpage url",
                 }
             ),
             "logo": forms.ClearableFileInput(
                 attrs={
                     "data-bs-toggle": "tooltip",
-                    "data-bs-placement": "top",
+                    "data-bs-placement": "left",
                     "title": "Project logo",
                 }
             ),
