75690ffb388ba50f241547f05d17a8c40f8d85cb
JunsongDu <junsong.du@eonerc.rwth-aachen.de>
2022-10-28 17:04:34 +0200

    fix: manual created superuser dose not have superuser access

--- app/Entirety/users/managers.py
+++ app/Entirety/users/managers.py
@@ -24,6 +24,7 @@ class UserManager(BaseUserManager):
         user.is_staff = True
         user.is_server_admin = True
         user.is_project_admin = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
