789d775a70b35051cadb54e261d0431686162cea
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2022-11-15 12:43:23 +0000

    Make website and logo optional in new project form

--- app/Entirety/projects/forms.py
+++ app/Entirety/projects/forms.py
@@ -12,6 +12,9 @@ class ProjectForm(forms.ModelForm):
 
         self.helper.layout.append(Submit(name="save", value="Save"))
 
+        self.fields["logo"].required = False
+        self.fields["webpage_url"].required = False
+
     class Meta:
         model = Project
         fields = ["name", "description", "fiware_service", "webpage_url", "logo"]
