ff00fc8ef17d7466058c8920404c94232d2fd7eb
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2022-12-06 16:49:29 +0100

    Add multi value field for entity/type pattern and id

--- app/Entirety/subscriptions/views/update.py
+++ app/Entirety/subscriptions/views/update.py
@@ -35,7 +35,11 @@ class Update(ProjectContextMixin, UpdateView):
         context = super().get_context_data(**kwargs)
         if self.request.POST:
             context["attributes"] = forms.AttributesForm(self.request.POST)
-            context["entities"] = forms.Entities(self.request.POST, prefix="entity")
+            context["entities"] = forms.Entities(
+                self.request.POST,
+                prefix="entity",
+                form_kwargs={"project": self.project},
+            )
         else:
             # Fill from context broker
             with ContextBrokerClient(
@@ -83,6 +87,7 @@ class Update(ProjectContextMixin, UpdateView):
                 form.initial[
                     "only_changed_attributes"
                 ] = cb_sub.notification.onlyChangedAttrs
+                form.initial["project"] = self.project
                 context["form"] = form
 
                 entities_initial = []
@@ -104,7 +109,9 @@ class Update(ProjectContextMixin, UpdateView):
                         }
                     )
                 context["entities"] = forms.Entities(
-                    prefix="entity", initial=entities_initial
+                    prefix="entity",
+                    initial=entities_initial,
+                    form_kwargs={"project": self.project},
                 )
                 attr_choices = utils.load_attributes(self.project, entities_initial)
                 context["attributes"] = forms.AttributesForm(
