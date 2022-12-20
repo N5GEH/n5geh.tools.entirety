38856ad60e9923e2a6d2630e1cf5fcc68539bf91
Saira Bano <saira.bano@eonerc.rwth-aachen.de>
2022-12-20 15:21:25 +0100

    fix: move common entirety list text widget

--- app/Entirety/entities/forms.py
+++ app/Entirety/entities/forms.py
@@ -2,24 +2,8 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Div, HTML
 from django import forms
 
+from entirety.widgets import ListTextWidget
 from entities.requests import AttributeTypes, get_entities_types
-
-
-class ListTextWidget(forms.TextInput):
-    def __init__(self, data_list, name, *args, **kwargs):
-        super(ListTextWidget, self).__init__(*args, **kwargs)
-        self._name = name
-        self._list = data_list
-        self.attrs.update({"list": "list__%s" % self._name, "autocomplete": "off"})
-
-    def render(self, name, value, attrs=None, renderer=None):
-        text_html = super(ListTextWidget, self).render(name, value, attrs=attrs)
-        data_list = '<datalist id="list__%s">' % self._name
-        for item in self._list:
-            data_list += '<option value="%s">' % item
-        data_list += "</datalist>"
-
-        return text_html + data_list
 
 
 class EntityForm(forms.Form):
