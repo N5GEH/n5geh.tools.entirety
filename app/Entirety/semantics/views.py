bb24bebb36687d2a39eccf67b6bcccdf3adc6045
Johannes Radebold <johannes.radebold@rwth-aachen.de>
2023-04-17 17:43:01 +0200

    add sementics views

--- app/Entirety/semantics/views.py
+++ app/Entirety/semantics/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
 from projects.models import Project
 from django.http import JsonResponse
 import json
@@ -12,15 +13,22 @@ class StartPage(ProjectContextMixin, TemplateView):
     template_name = "semantics/start_page.html"
 
 
-class SemanticsVisualizer(ProjectContextMixin,TemplateView):
+class SemanticsVisualizer(ProjectContextMixin, TemplateView):
     template_name = "semantics/semantics_visualize.html"
+
+
+
+
+
     def get_context_data(self, **kwargs):
+
         context = super().get_context_data(**kwargs)
+        print(get_entities_list(self, '.*', '', self.project))
         context['elements'] = generate_cytoscape_elements()
         context['types'] = types()
-        context['relationships']= relationships()
+        context['relationships'] = relationships()
         return context
+
 
 class LdVisualizer(ProjectContextMixin, TemplateView):
     templet_name = "semantics/semantics_LdVisualize.html"
-
