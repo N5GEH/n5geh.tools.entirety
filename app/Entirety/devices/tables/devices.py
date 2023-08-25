3d6b1894270dbfa4676136fb991b673fcf04ade1
JunsongDu <junsong.du@eonerc.rwth-aachen.de>
2023-08-25 09:59:23 +0200

    feat: add length limitation for columns in table

--- app/Entirety/devices/tables/devices.py
+++ app/Entirety/devices/tables/devices.py
@@ -1,4 +1,5 @@
 from django_tables2 import tables
+from entirety.utils import django_table2_limit_text_length
 
 
 class CheckBoxColumnWithName(tables.columns.CheckBoxColumn):
@@ -23,3 +24,13 @@ class DevicesTable(tables.Table):
     device_id = tables.columns.Column()
     entity_name = tables.columns.Column()
     entity_type = tables.columns.Column()
+    length_limit = 30
+
+    def render_device_id(self, value):
+        return django_table2_limit_text_length(value, length=self.length_limit)
+
+    def render_entity_name(self, value):
+        return django_table2_limit_text_length(value, length=self.length_limit)
+
+    def render_entity_type(self, value):
+        return django_table2_limit_text_length(value, length=self.length_limit)
