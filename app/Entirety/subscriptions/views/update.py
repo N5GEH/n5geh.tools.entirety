17046b247b23661f526d4522c1f7cf5fd3b6c67f
JunsongDu <junsong.du@eonerc.rwth-aachen.de>
2025-02-05 15:46:08 +0100

    fix(subscriptions): handle regex syntax error

--- app/Entirety/subscriptions/views/update.py
+++ app/Entirety/subscriptions/views/update.py
@@ -192,164 +192,177 @@ class Update(ProjectContextAndViewOnlyMixin, UpdateView):
         mqtt_custom = context["mqttCustom"]
 
         if form.is_valid() and entities_set.is_valid():
+            try:
+                data_set = [entity_form.cleaned_data for entity_form in entities_set]
+                # Otherwise choices are empty
+                attributes.fields["attributes"].choices = utils.load_attributes(
+                    self.project, data_set
+                )
+                if attributes.is_valid():
+                    form.save(commit=False)
 
-            data_set = [entity_form.cleaned_data for entity_form in entities_set]
-            # Otherwise choices are empty
-            attributes.fields["attributes"].choices = utils.load_attributes(
-                self.project, data_set
-            )
-            if attributes.is_valid():
-                form.save(commit=False)
+                    entities = []
 
-                entities = []
-
-                for entity_form in entities_set:
-                    if entity_form.cleaned_data:
-                        entity_selector = entity_form.cleaned_data["entity_selector"]
-                        type_selector = entity_form.cleaned_data["type_selector"]
-                        pattern = EntityPattern(
-                            id=(
-                                entity_form.cleaned_data["entity_id"]
-                                if entity_selector == "id"
-                                else None
-                            ),
-                            idPattern=(
-                                re.compile(entity_form.cleaned_data["entity_id"])
-                                if entity_selector == "id_pattern"
-                                else None
-                            ),
-                            type=(
-                                entity_form.cleaned_data["entity_type"]
-                                if entity_form.cleaned_data["entity_type"]
-                                and type_selector == "type"
-                                else None
-                            ),
-                            typePattern=(
-                                re.compile(entity_form.cleaned_data["entity_type"])
-                                if type_selector == "type_pattern"
-                                else None
-                            ),
-                        )
-                        entities.append(pattern)
-
-                with ContextBrokerClient(
-                    url=settings.CB_URL,
-                    fiware_header=FiwareHeader(
-                        service=self.project.fiware_service,
-                        service_path=self.project.fiware_service_path,
-                    ),
-                ) as cb_client:
-                    if (
-                        (
-                            form.cleaned_data["endpoint_type"] == "http"
-                            and http.is_valid()
-                        )
-                        or (
-                            form.cleaned_data["endpoint_type"] == "mqtt"
-                            and mqtt.is_valid()
-                        )
-                        or (
-                            form.cleaned_data["endpoint_type"] == "httpCustom"
-                            and http_custom.is_valid()
-                        )
-                        or (
-                            form.cleaned_data["endpoint_type"] == "mqttCustom"
-                            and mqtt_custom.is_valid()
-                        )
-                    ):
-                        try:
-                            cb_sub = cb_client.get_subscription(kwargs["pk"])
-                            cb_sub.description = form.cleaned_data["description"]
-                            cb_sub.throttling = form.cleaned_data["throttling"]
-                            cb_sub.expires = form.cleaned_data["expires"]
-                            cb_sub.subject = Subject(
-                                entities=entities,
-                                condition=Condition(
-                                    attrs=attributes.cleaned_data["attributes"]
+                    for entity_form in entities_set:
+                        if entity_form.cleaned_data:
+                            entity_selector = entity_form.cleaned_data[
+                                "entity_selector"
+                            ]
+                            type_selector = entity_form.cleaned_data["type_selector"]
+                            pattern = EntityPattern(
+                                id=(
+                                    entity_form.cleaned_data["entity_id"]
+                                    if entity_selector == "id"
+                                    else None
+                                ),
+                                idPattern=(
+                                    utils.safe_compile(
+                                        entity_form.cleaned_data["entity_id"]
+                                    )
+                                    if entity_selector == "id_pattern"
+                                    else None
+                                ),
+                                type=(
+                                    entity_form.cleaned_data["entity_type"]
+                                    if entity_form.cleaned_data["entity_type"]
+                                    and type_selector == "type"
+                                    else None
+                                ),
+                                typePattern=(
+                                    utils.safe_compile(
+                                        entity_form.cleaned_data["entity_type"]
+                                    )
+                                    if type_selector == "type_pattern"
+                                    else None
                                 ),
                             )
-                            cb_sub.notification = Notification(
-                                http=(
-                                    Http(**http.cleaned_data)
-                                    if form.cleaned_data["endpoint_type"] == "http"
-                                    else None
-                                ),
-                                httpCustom=(
-                                    HttpCustom(**http_custom.cleaned_data)
-                                    if form.cleaned_data["endpoint_type"]
-                                    == "httpCustom"
-                                    else None
-                                ),
-                                mqttCustom=(
-                                    MqttCustom(**mqtt_custom.cleaned_data)
-                                    if form.cleaned_data["endpoint_type"]
-                                    == "mqttCustom"
-                                    else None
-                                ),
-                                mqtt=(
-                                    Mqtt(**mqtt.cleaned_data)
-                                    if form.cleaned_data["endpoint_type"] == "mqtt"
-                                    else None
-                                ),
-                                metadata=(
-                                    form.cleaned_data["metadata"].split(",")
-                                    if form.cleaned_data["endpoint_type"] == "mqtt"
-                                    else None
-                                ),
-                                # attrs=form.cleaned_data["n_attributes"].split(",")
-                                # if form.cleaned_data["n_attributes"]
-                                # else None,
-                                # exceptAttrs=form.cleaned_data["n_except_attributes"].split(",")
-                                # if form.cleaned_data["n_except_attributes"]
-                                # else None,
-                                attrsFormat=form.cleaned_data["attributes_format"],
-                                onlyChangedAttrs=form.cleaned_data[
-                                    "only_changed_attributes"
-                                ],
-                            )
-                            # cb_sub.notification = Notification(
-                            #     http=(
-                            #         Http(url=form.cleaned_data["http"])
-                            #         if form.cleaned_data["http"]
-                            #         else None
-                            #     ),
-                            #     mqtt=(
-                            #         Mqtt(
-                            #             url=form.cleaned_data["mqtt"],
-                            #             topic=f"{settings.MQTT_BASE_TOPIC}/{self.project.uuid}",
-                            #         )
-                            #         if form.cleaned_data["mqtt"]
-                            #         else None
-                            #     ),
-                            #     metadata=(
-                            #         form.cleaned_data["metadata"].split(",")
-                            #         if form.cleaned_data["metadata"]
-                            #         else None
-                            #     ),
-                            #     # attrs=form.cleaned_data["n_attributes"].split(",")
-                            #     # if form.cleaned_data["n_attributes"]
-                            #     # else None,
-                            #     # exceptAttrs=form.cleaned_data["n_except_attributes"].split(",")
-                            #     # if form.cleaned_data["n_except_attributes"]
-                            #     # else None,
-                            #     attrsFormat=form.cleaned_data["attributes_format"],
-                            #     onlyChangedAttrs=form.cleaned_data["only_changed_attributes"],
-                            # )
-                            cb_client.update_subscription(cb_sub)
+                            entities.append(pattern)
+            except re.error as e:
+                logger.error(
+                    str(
+                        self.request.user.first_name
+                        if self.request.user.first_name
+                        else self.request.user.username
+                    )
+                    + " tried updating the subscription with name "
+                    + self.object.uuid
+                    + " but failed "
+                    f" in project {self.project.name}"
+                )
+                messages.error(request, e)
+                return self.form_invalid(form)
+            with ContextBrokerClient(
+                url=settings.CB_URL,
+                fiware_header=FiwareHeader(
+                    service=self.project.fiware_service,
+                    service_path=self.project.fiware_service_path,
+                ),
+            ) as cb_client:
+                if (
+                    (form.cleaned_data["endpoint_type"] == "http" and http.is_valid())
+                    or (
+                        form.cleaned_data["endpoint_type"] == "mqtt" and mqtt.is_valid()
+                    )
+                    or (
+                        form.cleaned_data["endpoint_type"] == "httpCustom"
+                        and http_custom.is_valid()
+                    )
+                    or (
+                        form.cleaned_data["endpoint_type"] == "mqttCustom"
+                        and mqtt_custom.is_valid()
+                    )
+                ):
+                    try:
+                        cb_sub = cb_client.get_subscription(kwargs["pk"])
+                        cb_sub.description = form.cleaned_data["description"]
+                        cb_sub.throttling = form.cleaned_data["throttling"]
+                        cb_sub.expires = form.cleaned_data["expires"]
+                        cb_sub.subject = Subject(
+                            entities=entities,
+                            condition=Condition(
+                                attrs=attributes.cleaned_data["attributes"]
+                            ),
+                        )
+                        cb_sub.notification = Notification(
+                            http=(
+                                Http(**http.cleaned_data)
+                                if form.cleaned_data["endpoint_type"] == "http"
+                                else None
+                            ),
+                            httpCustom=(
+                                HttpCustom(**http_custom.cleaned_data)
+                                if form.cleaned_data["endpoint_type"] == "httpCustom"
+                                else None
+                            ),
+                            mqttCustom=(
+                                MqttCustom(**mqtt_custom.cleaned_data)
+                                if form.cleaned_data["endpoint_type"] == "mqttCustom"
+                                else None
+                            ),
+                            mqtt=(
+                                Mqtt(**mqtt.cleaned_data)
+                                if form.cleaned_data["endpoint_type"] == "mqtt"
+                                else None
+                            ),
+                            metadata=(
+                                form.cleaned_data["metadata"].split(",")
+                                if form.cleaned_data["endpoint_type"] == "mqtt"
+                                else None
+                            ),
+                            # attrs=form.cleaned_data["n_attributes"].split(",")
+                            # if form.cleaned_data["n_attributes"]
+                            # else None,
+                            # exceptAttrs=form.cleaned_data["n_except_attributes"].split(",")
+                            # if form.cleaned_data["n_except_attributes"]
+                            # else None,
+                            attrsFormat=form.cleaned_data["attributes_format"],
+                            onlyChangedAttrs=form.cleaned_data[
+                                "only_changed_attributes"
+                            ],
+                        )
+                        # cb_sub.notification = Notification(
+                        #     http=(
+                        #         Http(url=form.cleaned_data["http"])
+                        #         if form.cleaned_data["http"]
+                        #         else None
+                        #     ),
+                        #     mqtt=(
+                        #         Mqtt(
+                        #             url=form.cleaned_data["mqtt"],
+                        #             topic=f"{settings.MQTT_BASE_TOPIC}/{self.project.uuid}",
+                        #         )
+                        #         if form.cleaned_data["mqtt"]
+                        #         else None
+                        #     ),
+                        #     metadata=(
+                        #         form.cleaned_data["metadata"].split(",")
+                        #         if form.cleaned_data["metadata"]
+                        #         else None
+                        #     ),
+                        #     # attrs=form.cleaned_data["n_attributes"].split(",")
+                        #     # if form.cleaned_data["n_attributes"]
+                        #     # else None,
+                        #     # exceptAttrs=form.cleaned_data["n_except_attributes"].split(",")
+                        #     # if form.cleaned_data["n_except_attributes"]
+                        #     # else None,
+                        #     attrsFormat=form.cleaned_data["attributes_format"],
+                        #     onlyChangedAttrs=form.cleaned_data["only_changed_attributes"],
+                        # )
+                        cb_client.update_subscription(cb_sub)
 
-                            logger.info(
-                                str(
-                                    self.request.user.first_name
-                                    if self.request.user.first_name
-                                    else self.request.user.username
-                                )
-                                + " has updated the subscription with name "
-                                + self.object.uuid
-                                + f" in project {self.project.name}"
+                        logger.info(
+                            str(
+                                self.request.user.first_name
+                                if self.request.user.first_name
+                                else self.request.user.username
                             )
-                            return self.form_valid(form)
-                        except Exception as e:
-                            messages.error(self.request, e)
+                            + " has updated the subscription with name "
+                            + self.object.uuid
+                            + f" in project {self.project.name}"
+                        )
+                        return self.form_valid(form)
+                    except Exception as e:
+                        messages.error(self.request, e)
 
         logger.error(
             str(
