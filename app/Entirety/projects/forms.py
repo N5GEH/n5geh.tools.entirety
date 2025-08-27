e1fb0c8a3a4761dd65d519d652f1912aeb518a26
Saira Bano <saira.bano@eonerc.rwth-aachen.de>
2025-08-27 09:00:21 +0200

    chore(projects): dashboard navigation and model change

--- app/Entirety/projects/forms.py
+++ app/Entirety/projects/forms.py
@@ -124,6 +124,7 @@ class ProjectForm(forms.ModelForm):
             "description",
             "fiware_service",
             "webpage_url",
+            "dashboard_url",
             "logo",
             "owner",
         ]
@@ -161,6 +162,13 @@ class ProjectForm(forms.ModelForm):
                     "data-bs-toggle": "tooltip",
                     "data-bs-placement": "left",
                     "title": "Project logo",
+                }
+            ),
+            "dashboard_url": forms.URLInput(
+                attrs={
+                    "data-bs-toggle": "tooltip",
+                    "data-bs-placement": "left",
+                    "title": "Project dashboard url",
                 }
             ),
         }
